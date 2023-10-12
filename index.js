70dd86d71ed862249eaae81eb1e3f5afd124f713
MarianaSko <maryana.skorynovych@gmail.com>
2023-10-12 14:52:16 +0300

    Task2

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // // Запитати у користувача "скільки апельсинів він хоче купити?",а потім вивести alert
 //     //"Користувач хоче купити <число> апельсинів!"
-const oranges = prompt('скільки апельсинів він хоче купити?');
-alert(`kористувач хоче купити ${oranges} апельсинів!`)
-console.log()
+// const oranges = prompt('скільки апельсинів він хоче купити?');
+// alert(`kористувач хоче купити ${oranges} апельсинів!`)
+// console.log()
+//TODO:===================================
+//Порахувати середню зарплатню співробітників і округлити число до сотих
+const Israel = '1600$';
+let Alexander = '2350$';
+const Dustin = '1344$';
+const israelSalary = Number.parseInt(Israel);
+const alexanderSalary = Number.parseInt(Alexander);
+const dustinSalary = Number.parseInt(Dustin);
+console.log(((israelSalary + alexanderSalary + dustinSalary) / 3).toFixed());
+Alexander = alexanderSalary + 200 + '$';
