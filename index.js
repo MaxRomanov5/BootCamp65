eb9f3cac001acb797c680c2b7f0c9794fb1f5d62
Alyonka556 <142902721+Alyonka556@users.noreply.github.com>
2023-10-13 13:27:49 +0200

    update

--- index.js
+++ index.js
@@ -5,18 +5,30 @@
 // console.log()
 //TODO:===================================
 //Порахувати середню зарплатню співробітників і округлити число до сотих
-const Israel = '1600$';
-let Alexander = '2350$';
-const Dustin = '1344$';
-const israelSalary = Number.parseInt(Israel);
-const alexanderSalary = Number.parseInt(Alexander);
-const dustinSalary = Number.parseInt(Dustin);
-console.log(((israelSalary + alexanderSalary + dustinSalary) / 3).toFixed());
-Alexander = alexanderSalary + 200 + '$';
+// const Israel = '1600$';
+// let Alexander = '2350$';
+// const Dustin = '1344$';
+// const israelSalary = Number.parseInt(Israel);
+// const alexanderSalary = Number.parseInt(Alexander);
+// const dustinSalary = Number.parseInt(Dustin);
+// console.log(((israelSalary + alexanderSalary + dustinSalary) / 3).toFixed());
+// Alexander = alexanderSalary + 200 + '$';
 //Знайти в рядку слово '//Знайти в рядку слово 'spam' та замінити його на '@@@@'
 
 //const str = 'I gEt sO much sPam at wOrk.';' та замінити його на '@@@@'
 
-const str = 'I gEt sO much sPam at wOrk.';
-const correctStr = str.toLowerCase().replace('spam', '@@@@');
-console.log(correctStr);
+// const str = 'I gEt sO much sPam at wOrk.';
+// const correctStr = str.toLowerCase().replace('spam', '@@@@');
+// console.log(correctStr);
+
+// TODO:===================================
+// Напишіть цикл, який виводить у консоль
+// числа від max до min за спаданням
+// Додайте усі парні числа від min до max
+
+const max = 50;
+const min = 23;
+
+for (let index = max; index > min; index-=1) {
+    console.log(index); 
+}
